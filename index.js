b65e25debbf1bbd3a747b9d79261a8a7f8977a1c
rzemlin <34727847+rzemlin@users.noreply.github.com>
2022-04-11 09:43:53 -0700

    add post form

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ const endPoint = "http://localhost:3000/api/v1/channels"
 
 document.addEventListener("DOMContentLoaded", () => {
 getChannels()
+
+const createChannelForm = document.querySelector("#create-channel-form")
+  createChannelForm.addEventListener("submit", (e) => createFormHandler(e))
+
 })
+
 
 function getChannels() {
     fetch(endPoint)
 .then(response => response.json())
 .then(channels => {
     channels.data.forEach(channels => {
-        // double check how your data is nested in the console so you can successfully access the attributes of each individual object
+      
         const channelsMarkup = `
           <div data-id=${channels.id}>
             <img src=${channels.attributes.image_url} height="200" width="250">
@@ -22,4 +27,18 @@ function getChannels() {
           document.querySelector('#channels-container').innerHTML += channelsMarkup
       })
   })
+}
+
+function createFormHandler(e) {
+    e.preventDefault()
+    const titleInput = document.querySelector("#input-title").value
+    const descriptionInput = document.querySelector("#input-description").value
+    const img_urlInput = document.querySelector("#input-url").value
+    const genreId = parseInt(document.querySelector("#genre").value)
+    postFetch(titleInput, descriptionInput, img_urlInput, genreId)
+}
+
+function postFetch(title, description, img_url, genre_id) {
+console.log(title, description, img_url, genre_id)
+
 }
