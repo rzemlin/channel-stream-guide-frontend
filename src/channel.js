34072dadaeb21d4ad06d5968a46d988ab3b3d843
rzemlin <34727847+rzemlin@users.noreply.github.com>
2022-04-29 17:18:11 -0700

    implement oojs and modify render function

---
+++ src/channel.js
@@ -0,0 +1,26 @@
+class Channel {
+
+    constructor(channel, channelAttributes) {
+      this.id = channel.id
+      this.name = channelAttributes.name
+      this.description = channelAttributes.description
+      this.img_url = channelAttributes.img_url
+      this.genre = channelAttributes.genre
+      Channel.all.push(this)
+      // debugger
+    }
+    
+    renderChannelCard() {
+         // debugger
+        return `
+        <div data-id=${this.id}>
+          <img src=${this.image_url} height="200" width="250">
+          <h3>${this.name}</h3>
+          <p>${this.genre.name}</p>
+          <button data-id=${this.id}>edit</button>
+        </div>
+        <br><br>`;
+      }
+}
+
+Channel.all = [];
